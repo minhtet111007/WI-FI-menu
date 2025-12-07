9ad2fbc2bd53547d040f742bb880e1cf74056385
Jerry <jerry.207@outlook.com>
2025-12-07 12:18:03 +0630

    Day 2 finishing it

---
+++ README.md
@@ -0,0 +1,9 @@
+# WI-FI menu written in Bash
+
+- This little script can run in terminal and be used to select network.
+- But due to being a beginner, I've tried my best.
+- The first line shebang can be changed, so it's better to `which bash` before you use
+- If you can't input a password to unsaved network, this may due to nmcli's saving connection that you've entered with invalid password.
+- To solve this, `nmcli connection delete ssid`.
+
+
--- menu.sh
+++ menu.sh
@@ -3,18 +3,25 @@
 # Use "which bash" command
 saved_wifi=$(nmcli connection show | grep "wifi")
 
-selected_wifi=$(nmcli -f SSID dev wifi list | tail -n +2 | fzf | xargs)
+selected_wifi=$(nmcli -f SSID dev wifi list | tail -n +2 | fzf --height=10 | xargs)
 # tail to skip the first line
 # xargs to trim
 
 # Grep the saved wifi network
 saved_wifi=$(nmcli connection show | grep "$selected_wifi")
-
-if [ "$saved_wifi" == "" ]; then
-    echo "Connecting to $selected_wifi"
-    read -p "Password: " password
-    nmcli dev wifi connect "$selected_wifi" password "$password"
+if [ "$selected_wifi" == "" ]; then
+    echo "Invalid input!"
 else
-    echo "Connecting to $selected_wifi..."
-    nmcli dev wifi connect "$selected_wifi"
+    if [ "$saved_wifi" == "" ]; then
+        echo "Connecting to $selected_wifi..."
+        read -p "Password: " -s password
+        nmcli dev wifi connect "$selected_wifi" password "$password"
+        if [ "$?" -gt 0 ]; then
+            echo "Invalid password!"
+            nmcli connection delete "$selected_wifi"
+        fi
+    else
+        echo "Connecting to $selected_wifi..."
+        nmcli dev wifi connect "$selected_wifi"
+    fi
 fi
