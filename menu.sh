a272d8338696741ce272a8113d80d8976ecfc9c1
Jerry <jerry.207@outlook.com>
2025-12-12 11:47:55 +0630

    Some updates!

--- menu.sh
+++ menu.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 # Change the above as you need
 # Use "which bash" command
-saved_wifi=$(nmcli connection show | grep "wifi")
+
+if [ $(nmcli networking) == "disabled" ]; then
+    echo "Enable your network!"
+fi
 
 selected_wifi=$(nmcli -f SSID dev wifi list | tail -n +2 | fzf --height=10 | xargs)
-# tail to skip the first line
-# xargs to trim
-
-# Grep the saved wifi network
 saved_wifi=$(nmcli connection show | grep "$selected_wifi")
+
+save_wifi() {
+        echo "Connecting to $selected_wifi..."
+        read -p "Password: " -s password
+        nmcli dev wifi connect "$selected_wifi" password "$password" > /dev/null
+        if [ "$?" -gt 0 ]; then
+            echo "Invalid password!"
+            nmcli connection delete "$selected_wifi" > /dev/null
+            save_wifi
+        else 
+            echo "Wifi Connected!"
+        fi
+}
+
 if [ "$selected_wifi" == "" ]; then
     echo "Invalid input!"
 else
     if [ "$saved_wifi" == "" ]; then
-        echo "Connecting to $selected_wifi..."
-        read -p "Password: " -s password
-        nmcli dev wifi connect "$selected_wifi" password "$password"
-        if [ "$?" -gt 0 ]; then
-            echo "Invalid password!"
-            nmcli connection delete "$selected_wifi"
-        fi
+        save_wifi
     else
         echo "Connecting to $selected_wifi..."
-        nmcli dev wifi connect "$selected_wifi"
+        nmcli dev wifi connect "$selected_wifi" > /dev/null
+        if [ "$?" -gt 1 ]; then
+            read -p "Do you want to forget and re-enter password? [y/N]" input
+            if [ "$input" == "y" ]; then
+                nmcli connection delete "$selected_wifi" > /dev/null
+                save_wifi
+            else
+                echo "Can't connect to $selected_wifi"
+            fi
+        else
+            echo "Wifi Connected!"
+        fi
     fi
 fi
